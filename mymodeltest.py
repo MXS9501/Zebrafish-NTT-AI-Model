22b7921b1f58cf52c45cc952ab86bc5d664fe9ef
MXS9501 <bio_cui@outlook.com>
2025-01-23 02:49:29 +0800

    confirm

---
+++ mymodeltest.py
@@ -0,0 +1,22 @@
+from ultralytics import YOLO
+import cv2
+import glob
+
+# Load a pretrained YOLO model
+model = YOLO("D:/Test_File/Code/Zebrafish-NTT-AI-Model/runs/detect/train6/weights/best.pt")
+# Define path to the image files
+source = "D:/Test_File/Code/Zebrafish-NTT-AI-Model/mydataset/test/*.jpg"
+
+# Get list of image files
+image_files = glob.glob(source)
+
+# Run inference on each image and display results
+for image_file in image_files:
+    results = model(image_file)  # list of Results objects
+    result_image = results[0].plot()  # get the image with results
+
+    # Display the result image
+    cv2.imshow('Result', result_image)
+    cv2.waitKey(0)  # Wait for a key press to show the next image
+
+cv2.destroyAllWindows()
