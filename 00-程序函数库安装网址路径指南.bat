3f82df9c86e769207f17d40bd773c3d7398000a8
MXS9501 <bio_cui@outlook.com>
2025-02-15 18:30:14 +0800

    Confirm

---
+++ 00-程序函数库安装网址路径指南.bat
@@ -0,0 +1,25 @@
+# 视觉处理库，识别黑线，分割视频
+# https://opencv.org/get-started/?utm_source=opcv&utm_medium=home
+pip3 install opencv-python
+
+# UI库
+# https://pypi.org/project/PyQt5/
+pip3 install PyQt5
+
+# alive_progrerss进度条
+pip3 install alive_progress
+
+# 安装 ultralytics YOLOv11
+pip install ultralytics
+
+# Matplotlib: Visualization with Python
+pip3 install matplotlib
+
+# 安装torch (CPU)
+# https://pytorch.org/get-started/locally/
+pip3 install torch torchvision torchaudio
+
+# 安装标注工具 label studio
+# https://labelstud.io/
+pip3 install label-studio
+# label-studio start
